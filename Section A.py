a354aad44b994959804ec4e6c5e1dbfc3d684994
Aidan-Wisnia <45011796+Aidan-Wisnia@users.noreply.github.com>
2018-11-19 16:04:23 +0000

    Got up to Vloop
    Could not get it to work for some reaon

--- Section A.py
+++ Section A.py
@@ -62,10 +62,100 @@ v2bd = []
 v2be = []
 for n in range(0,k):
     for i in range(0,3):
-        v2f.append(A * dof[0] * ((x[n] * xd[i]) ** 4) * (np.log(((x * xd[i]) ** 2 / Q ** 2) - 1.5)))
+        v2f.append(A * -(dof[0] * ((x[n] * xd[i]) ** 4) * (np.log(((x * xd[i]) ** 2 / Q ** 2) - 1.5))))
         v2ba.append(A * dof[1] * ((mxa[n] * xd[i]) ** 4) * (np.log((((mxa[n] * xd[i]) ** 2) / (Q ** 2)) - 1.5)))
         v2bb.append(A * dof[2] * ((mxb[n] * xd[i]) ** 4) * (np.log((((mxb[n] * xd[i]) ** 2) / (Q ** 2)) - 1.5)))
         v2bc.append(A * dof[3] * ((mxc[n] * xd[i]) ** 4) * (np.log((((mxc[n] * xd[i]) ** 2) / (Q ** 2)) - 1.5)))
         v2bd.append(A * dof[4] * ((mxd[n] * xd[i]) ** 4) * (np.log((((mxd[n] * xd[i]) ** 2) / (Q ** 2)) - 1.5)))
         v2be.append(A * dof[5] * ((mxe[n] * xd[i]) ** 4) * (np.log((((mxe[n] * xd[i]) ** 2) / (Q ** 2)) - 1.5)))
+#For boson A:
+v2x0ba = v2ba[0]+v2f[0]
+v2xpba = v2ba[1]+v2f[1]
+v2xnba = v2ba[2]+v2f[2]
+
+dfv2a = (v2xpba-v2x0ba)/h
+d2fv2a = ((v2xpba - (2*v2x0ba) + v2xnba)/(h**2))
+
+#dl and dm calculation:
+dlba = 0.5*(((1/(xd[0]**3))*dfv2a) - ((1/(xd[0]**2))*d2fv2a))
+dm2a = -d2fv2a - (3*dlba*xd[0]**2)
+
+#For boson B:
+v2x0bb = v2bb[0]+v2f[0]
+v2xpbb = v2bb[1]+v2f[1]
+v2xnbb = v2bb[2]+v2f[2]
+
+dfv2b = (v2xpbb-v2x0bb)/h
+d2fv2b = ((v2xpbb - (2*v2x0bb) + v2xnbb)/(h**2))
+
+#dl and dm calculation:
+dlbb = 0.5*(((1/(xd[0]**3))*dfv2b) - ((1/(xd[0]**2))*d2fv2b))
+dm2b = -d2fv2b - (3*dlbb*xd[0]**2)
+
+#For boson C:
+v2x0bc = v2bc[0]+v2f[0]
+v2xpbc = v2bc[1]+v2f[1]
+v2xnbc = v2bc[2]+v2f[2]
+
+dfv2c = (v2xpbc-v2x0bc)/h
+d2fv2c = ((v2xpbc - (2*v2x0bc) + v2xnbc)/(h**2))
+
+#dl and dm calculation:
+dlbc = 0.5*(((1/(xd[0]**3))*dfv2c) - ((1/(xd[0]**2))*d2fv2c))
+dm2c = -d2fv2c - (3*dlbc*xd[0]**2)
+
+#For boson D:
+v2x0bd = v2bd[0]+v2f[0]
+v2xpbd = v2bd[1]+v2f[1]
+v2xnbd = v2bd[2]+v2f[2]
+
+dfv2d = (v2xpbd-v2x0bd)/h
+d2fv2d = ((v2xpbd - (2*v2x0bd) + v2xnbd)/(h**2))
+
+#dl and dm calculation:
+dlbd = 0.5*(((1/(xd[0]**3))*dfv2d) - ((1/(xd[0]**2))*d2fv2d))
+dm2d = -d2fv2d - (3*dlbd*xd[0]**2)
+
+#For boson E:
+v2x0be = v2be[0]+v2f[0]
+v2xpbe = v2be[1]+v2f[1]
+v2xnbe = v2be[2]+v2f[2]
+
+dfv2e = (v2xpbe-v2x0be)/h
+d2fv2e = ((v2xpbe - (2*v2x0be) + v2xnbe)/(h**2))
+
+#dl and dm calculation:
+dlbe = 0.5*(((1/(xd[0]**3))*dfv2e) - ((1/(xd[0]**2))*d2fv2e))
+dm2e = -d2fv2e - (3*dlbe*xd[0]**2)
+
+#Now to insert these new found dl and dm into the V2(counter term):
+
+v2ba1 = []
+v2bb1 = []
+v2bc1 = []
+v2bd1 = []
+v2be1 = []
+
+for i in range(0,k):
+    v2ba1.append((((dm2a ** 2) / 2) * (x[i] ** 2)) + (((dlba ** 4) / 4) * (x[i] ** 4)))
+    v2bb1.append((((dm2b ** 2) / 2) * (x[i] ** 2)) + (((dlbb ** 4) / 4) * (x[i] ** 4)))
+    v2bc1.append((((dm2c ** 2) / 2) * (x[i] ** 2)) + (((dlbc ** 4) / 4) * (x[i] ** 4)))
+    v2bd1.append((((dm2d ** 2) / 2) * (x[i] ** 2)) + (((dlbd ** 4) / 4) * (x[i] ** 4)))
+    v2be1.append((((dm2e ** 2) / 2) * (x[i] ** 2)) + (((dlbe ** 4) / 4) * (x[i] ** 4)))
+
+
+#Now to make the Vloop for the 5 bosons:
+vloopa = []
+vloopb = []
+vloopc = []
+vloopd = []
+vloope = []
+
+for i in range(0,k):
+    vloopa.append(v0[i] + v1ba[i] + v2ba1[i])
+    vloopb.append(v0[i] + v1bb[i] + v2bb1[i])
+    vloopc.append(v0[i] + v1bc[i] + v2bc1[i])
+    vloopd.append(v0[i] + v1bd[i] + v2bd1[i])
+    vloope.append(v0[i] + v1be[i] + v2be1[i])
+
 
