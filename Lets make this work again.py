c97cdce0f8775f079d3fa8df3bcf413d38dc81dd
Unknown <squidey57@gmail.com>
2018-11-28 15:28:06 +0000

    Fermion graph works
    But Boson doesnt't

--- Lets make this work again.py
+++ Lets make this work again.py
@@ -6,13 +6,14 @@ from scipy import integrate
 
 l = 1 / 8
 m = np.sqrt(1 / 8)
-x = np.linspace(0, 2, 400)
-t = np.ones(400)
-mx = 1
+k = 1200
+x = np.linspace(0, 6, k)
+t = np.ones(k)
+mx = 3
 dox = 3
 A = (1 / (64 * np.pi ** 2))
 Q = 1
-mf = 2
+mf = 1
 dof = 4
 h = 0.01
 xd = np.array([1, (1 + h), (1 - h)])
@@ -21,8 +22,8 @@ uv2b = []
 uv2f = []
 
 for i in range(0, 3):
-    uv2b.append(A * dox * ((mx * xd[i]) ** 4) * ((np.log(((mx * xd[i]) ** 2) / Q ** 2)) - 1.5))
-    uv2f.append(-A * dof * ((mf * xd[i]) ** 4) * (np.log(((mf * xd[i] ** 2) / Q ** 2)) - 1.5))
+    uv2b.append(A * dox * ((mx * xd[i]) ** 4) * (np.log(((mx * xd[i]) ** 2) / Q ** 2) - 1.5))
+    uv2f.append(-A * dof * ((mf * xd[i]) ** 4) * (np.log(((mf * xd[i]) ** 2) / Q ** 2) - 1.5))
 
 v2x0b1 = uv2b[0]
 v2xpb1 = uv2b[1]
@@ -46,7 +47,7 @@ def vnloop(x):
            (-A * dof * ((mf * x) ** 4) * (np.log(((mf * x ** 2) / Q ** 2)) - 1.5)) + \
            (((dm2b / 2) * x ** 2) + ((dlb / 4) * x ** 4)) + (((dm2f / 2) * x ** 2) + ((dlf / 4) * x ** 4))
 
-T = 0.75
+T = 2
 def intb(r):
     return (r**2) * np.log(1-np.exp(-np.sqrt((r**2) + ((mx*x[i])/T)**2)))
 
@@ -55,14 +56,14 @@ def intf(r):
 
 i3b = []
 i3f = []
-uv3b = np.zeros(400)
-uv3f = np.zeros(400)
-eb = np.zeros(400)
-ef = np.zeros(400)
+uv3b = np.zeros(k)
+uv3f = np.zeros(k)
+eb = np.zeros(k)
+ef = np.zeros(k)
 v3b = []
 v3f = []
 v3 = []
-for i in range(0,400):
+for i in range(0,k):
     i3b.append(sci.integrate.quad(intb,0,np.inf))
     i3f.append(sci.integrate.quad(intf,0,np.inf))
     uv3b[i], eb[i] = i3b[i]
@@ -77,10 +78,19 @@ Vtot1 = vnloop(x[1]) + v3[1]
 print(np.min(Vtot1))
 pylab.plot(vnloop(x) , label = 'VnLoop')
 pylab.plot(Vtot - Vtot1, label = 'VTotal')
-pylab.ylim(-0.00005,0.00005)
-pylab.xlim(0,180)
+pylab.ylim(-0.1,0.1)
+pylab.xlim(400,600)
 pylab.legend()
 pylab.show()
 
+
+v_tf = np.array([(112.5/200)/(0.6175), (48/200)/(0.7572), (20/200)/(1.1125), (10/200)/1.5035, (8/200)/1.8994])
+v_tb = np.array([(113/200)/(0.6175), (337/200)/0.815, (490/200)/2.008, (569/200)/3.782, (672/200)/5.02])
+k = np.array([1,2,3,4,5])
+
+#pylab.plot(k, v_tf, label = 'Fermion Mass Variation')
+#pylab.plot(k, v_tb, label = 'Boson Mass Variation')
+#pylab.legend()
+#pylab.show()
 
 
